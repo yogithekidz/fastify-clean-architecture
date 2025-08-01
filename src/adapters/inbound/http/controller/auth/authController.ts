0b830d9795d1e49ca7b175480d84fa7ce843b63c
Hanafi Adhi <macbookpro2015@macbooks-MacBook-Pro.local>
2025-08-01 14:58:17 +0700

    refac(): route and many more
    Signed-off-by: Hanafi Adhi <macbookpro2015@macbooks-MacBook-Pro.local>

--- src/adapters/inbound/http/controller/auth/authController.ts
+++ src/adapters/inbound/http/controller/auth/authController.ts
@@ -3,9 +3,10 @@ import { loginUser } from '@application/services/User/UserService';
 import { loginSchema } from '@utils/password/password';
 
 export const loginHandler = async (req: FastifyRequest, reply: FastifyReply) => {
+  return reply.status(400).send("GG");
   const { error, value } = loginSchema.validate(req.body);
   if (error) {
-    return reply.status(400).send({ message: error.message });
+    // return reply.status(400).send({ message: error.message });
   }
 
   try {
@@ -15,3 +16,14 @@ export const loginHandler = async (req: FastifyRequest, reply: FastifyReply) => 
     return reply.status(401).send({ message: err.message });
   }
 };
+
+ /**
+  * penulisan functionya bisa langgsung aja
+  *  export async loginHandler() {}
+  *
+  * untuk request.body jangan langsung di validasi cuma di berikan typenya saja
+  * ex:
+  * const userLogin = req.body as UserRequestLoginDto
+  * as disini untuk memberi tahu bahwa userLogin itu punya type yang sudah di tentukan semacam type casting namanya
+  * style yang elu bikin engga salah juga cuma disini code stylenya seperti yang di consothkan jadi mohon di ganti
+  */
--- src/adapters/inbound/http/routes/auth.ts
+++ src/adapters/inbound/http/routes/auth.ts
@@ -1,8 +1,44 @@
-import { FastifyPluginAsync } from 'fastify';
-import { loginHandler } from '@adapters/inbound/http/controller/auth/AuthController';
+import { FastifyInstance, FastifyPluginOptions, RouteOptions } from 'fastify';
+// import { loginHandler } from '@adapters/inbound/http/controller/auth/AuthController';
+import { AuthController } from "../controller/auth";
+// const authRoutes: FastifyPluginAsync = async (fastify) => {
+//   fastify.post('/login', loginHandler);
+// };
 
-const authRoutes: FastifyPluginAsync = async (fastify) => {
-  fastify.post('/login', loginHandler);
-};
+// export default authRoutes;
 
-export default authRoutes;
+const routes: RouteOptions[] = [
+  {
+    method: ["GET"],
+    url: "/api/v1/admin/test",
+    // preHandler: CheckRules({ rules: Rules.MMB_CAN_VIEW_MENU }),
+    handler: async () => {
+      return "Hello world";
+    },
+  },
+  {
+    method: ["POST"],
+    url: "/api/v1/login",
+    // preHandler: CheckRules({ rules: Rules.MMB_CAN_VIEW_MENU }), -> biasanya di gunakan untuk cke role
+    handler: AuthController.loginHandler
+    /**
+     * untuk schema di sesuaikan saja bisa  sesuai kebutuhan
+     */
+  //   schema: {
+  //     tags: ["Admin Whitelabel"],
+  //     description: "Restore Trashed User",
+  //     summary: "Restore Trashed User",
+  //     body: Schema.BaseRequestSchema("Rakha", { user_id: { type: "integer" } }),
+  //     response: Schema.BaseResponse({
+  //         type: "Boolean",
+  //     })
+  // },
+},
+];
+
+export default async function FastifyAuthPluginRoute(fastify: FastifyInstance, options: FastifyPluginOptions) {
+  // fastify.addHook("preValidation", MMBWhitelabelAuthValidate);
+  for (const route of routes) {
+      fastify.route({ ...route, config: options });
+  }
+}
